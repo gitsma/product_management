d51e0aefca5ccf7f4bf1a94895160237b3b8005c
Gitana Tumeniene <47040023+gitsma@users.noreply.github.com>
2023-05-11 12:58:44 +0300

    Add files via upload

--- js.js
+++ js.js
@@ -15,22 +15,25 @@ let enterName = document.getElementById("enterName");
 let enterQuantity = document.getElementById("enterQuantity");
 let findID = document.getElementById("findID");
 let result = document.getElementById("result");
+let enterURL = document.getElementById("enterURL");
+let placeImage = document.getElementById("placeImage")
 
 let insertBtn = document.getElementById("insert");
 let updateBtn = document.getElementById("update");
 let removeBtn = document.getElementById("remove");
 let findBtn = document.getElementById("find");
 
-let findDataPlace = document.getElementById("findDataPlace");
+
 
 function insertData(evt){
     evt.preventDefault();
-    console.log(typeof enterID.value.length, enterName.value, enterQuantity.value.length);
+    console.log(typeof enterID.value.length, enterName.value, enterQuantity.value.length, enterURL.value);
 
     set(ref(database, "products/" + enterID.value), {
         Name: enterName.value,
         ID: enterID.value,
-        Quantity: enterQuantity.value
+        Quantity: enterQuantity.value,
+        Image: enterURL.value
     })
         .then(()=> {
             alert("data added successfully");
@@ -39,8 +42,8 @@ function insertData(evt){
             alert(error);
         });
 }
-
 insertBtn.addEventListener('click', insertData);
+
 
 function findData(evt){
     evt.preventDefault();
@@ -55,7 +58,17 @@ function findData(evt){
         .then((snapshot) => {
             if (snapshot.exists()){
                 console.log(snapshot.val());
-                findDataPlace.append(JSON.stringify(snapshot.val()));
+                let listItem = document.createElement('li');
+                listItem.classList.add("list-group-item", "list-group-item-secondary");
+                listItem.textContent = "Product Name: " + snapshot.val().Name;
+                result.appendChild(listItem);
+                let listItemSecond = document.createElement('li');
+                listItemSecond.classList.add("list-group-item", "list-group-item-light");
+                listItemSecond.textContent = "Product Quantity: " + snapshot.val().Quantity;
+                result.appendChild(listItemSecond);
+                let listItemImage = document.createElement('img');
+                listItemImage.src = snapshot.val().Image;
+                placeImage.appendChild(listItemImage);
 
             } else {
                 alert("No data found");
@@ -67,3 +80,63 @@ function findData(evt){
 }
 
 findBtn.addEventListener('click', findData);
+
+function updateData(evt) {
+    evt.preventDefault();
+    if (enterID.value.length < 3){
+        alert('Product Code cant be blank! MIN 3 Symbols')
+        return
+    }
+    if (enterName.value === "") {
+        alert('Product Name cant be blank!')
+        return
+    }
+    if (enterQuantity.value.length < 1){
+        alert('Product Quantity cant be blank! MIN 1 Symbols')
+        return
+    }
+    console.log(`update function ${enterID.value}
+                                ${enterName.value}
+                                ${enterQuantity.value}`);
+    update(ref(database, "products/" + enterID.value), {
+        Name: enterName.value,
+        Quantity: enterQuantity.value
+    })
+    .then(() => {
+        alert("Data updated successfully");
+    })
+    .catch((error) => {
+        alert(error);
+    });
+}
+
+updateBtn.addEventListener('click', updateData);
+
+
+function removeData(evt) {
+    evt.preventDefault();
+    if (enterID.value.length < 3) {
+        alert('Product code cant be blank! MIN 3 Symbols')
+        return
+    }
+    const dbRef = ref(database);
+    get(child(dbRef, `products/${enterID.value}`)).then((snapshot) => {
+        if (snapshot.exists()) {
+            console.log(snapshot.val());
+            remove(ref(database, "products/" + enterID.value))
+            .then(() => {
+                alert("Data deleted successfully");
+            })
+            .catch((error) => {
+                alert(error);
+            });
+        } else {
+            console.log("No data available");
+        }
+    })
+    .catch((error) => {
+        alert(error);
+    });
+}
+
+removeBtn.addEventListener('click', removeData);
